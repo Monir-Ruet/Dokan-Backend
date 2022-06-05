a7587dc7b51fbd9c77b332918724373d5b1b2a22
Monir <monir81.mk@gmail.com>
2022-06-05 22:14:39 +0600

    Deploy

--- src/index.js
+++ src/index.js
@@ -8,16 +8,26 @@ require('dotenv').config()
 const express=require('express');
 const cors=require('cors')
 const app=express();
+const http = require('http').Server(app);
 const signin=require('../Router/SignIn/SignIn');
 const Product=require('../Router/Product/Product.js')
 const singup=require('../Router/SignUp/SignUp')
 const LoggedUser=require('../Router/Logged/loggedUser')
-app.listen(process.env.PORT || 3000.,()=>{
+const {isAuthorized}=require('../Router/Authentication/Authentication.js')
+
+var io = require('socket.io')(http,{
+  'pingTimeout': 7000, 'pingInterval': 3000,
+  cors: {
+    origin: "*",
+  }
+})
+http.listen(process.env.PORT || 3000.,()=>{
   console.log("[+] Server Running...")
 });
 
 // Middlewares
 app.use(cors())
+app.use(express.json())
 app.use('/products',Product)
 app.use('/login',signin)
 app.use('/signup',singup)
@@ -25,6 +35,9 @@ app.use('/loggedUser',LoggedUser);
 
 app.get('/',(req,res)=>{
   res.send("This is the dokan Backend");
+})
+app.post('/notify',isAuthorized,(req,res)=>{
+  if(req.role==='Admin') io.emit('notify',req.body.Code);
 })
 app.use((err, req, res, next) => {
   if(err instanceof SyntaxError && err.status===400 && 'body' in err) return res.status(400).send({Status:400,Message:"Bad Request"});
